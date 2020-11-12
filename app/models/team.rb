5653fb54176a237a4ae3e945385096e3d6e1adbd
Bobby BuffaloBoy <novenarii@gmail.com>
2020-11-12 15:08:43 -0600

    updated team and project controllers, added before_actions, updated params, and updated new/ create methods

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,5 +3,5 @@ class Team < ApplicationRecord
 
     has_many :users
 
-    accept_nested_attributes_for :user, allow_destroy :true
+    accept_nested_attributes_for :users, allow_destroy :true
 end
